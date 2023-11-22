3cef2b8283ecdd1fd756dcf9d52a844274cc1b7c
Phuc Nguyen <fedor@linecorp.com>
2023-11-22 10:35:01 +0700

    Initial Commit

---
+++ SwiftUINavigation/SwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationApp.swift
+//  SwiftUINavigation
+//
+//  Created by Nguyen Hoang Phuc on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
